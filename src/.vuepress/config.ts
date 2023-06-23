f19c7749e70fd628dbca41242adf6d84314070ad
chovy <601119280@qq.com>
2023-06-23 22:45:28 +0800

    修复网络图片加载不了的问题

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -7,6 +7,16 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: "luoluo",
   description: "luoluo 's blog",
+  // 加载网络图片配置
+  head: [
+    [
+      "meta",
+      {
+        name: "referrer",
+        content: "no-referrer"
+      }
+    ],
+  ],
 
   theme,
 
